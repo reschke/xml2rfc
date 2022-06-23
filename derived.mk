58a3b3339c73379529185b9a0bee93a916094d8b
Julian Reschke <julian.reschke@gmx.de>
2022-06-23 07:34:36 +0000

    *** empty log message ***

--- derived.mk
+++ derived.mk
@@ -1,3 +1,3 @@
-rfc2629-no-doctype.xslt: rfc2629.xslt
+rfc2629-no-doctype.xslt: rfcxml.xslt
 	fgrep -v "<xsl:output method=\"html\" encoding=\"utf-8\" doctype-system=\"about:legacy-compat\" indent=\"no\"/>" $< > $@
 
